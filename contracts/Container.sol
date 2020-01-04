daf8946d06dcbf95ebd7e25c99393f276e3d2615
Dapeng Hu <damon.hu@gmail.com>
2020-01-04 23:10:54 +0800

    create container contract

---
+++ contracts/Container.sol
@@ -0,0 +1,78 @@
+pragma solidity ^0.5.0;
+
+import "@openzeppelin/upgrades/contracts/Initializable.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/cryptography/ECDSA.sol";
+import "./RelayerRole.sol";
+
+/* 
+ * @dev 联盟容器，负责管理联盟内的合约、账户、交易中继
+ */
+contract Container is Initializable, Ownable, RelayerRole{
+    using ECDSA for bytes32;
+
+    /**  @dev init: ERC20Detailed.initialize(), RelayerRole.initialize()
+     *  called by app deployer
+     */
+    function initialize(address relayer) public initializer {
+        Ownable.initialize(msg.sender);
+        RelayerRole.initialize(relayer);
+    }
+
+    event TransactionMeta(bytes metaHash, bytes metaSignature);
+    event TransferEvent(address token, address sender, address recipient, uint256 amount, bytes32 metaHash);
+    
+    function relayTransfer(
+        address tokenAddress,
+        address sender,
+        address recipient,
+        uint256 amount,
+        bytes32 metaHash,
+        bytes memory metaSign
+    ) public onlyRelayer(msg.sender) returns(bool) {
+        // verify parameters
+        require(tokenAddress != address(0), "token address is invalide");
+        require(sender != address(0), "sender address is invalide");
+        require(recipient != address(0), "recipient address is invalide");
+
+        // verify sender's signature
+        address result = metaHash.recover(metaSign);
+        require(result == sender, "sender's signature is invalid");
+
+        // encode transfer argument
+        bytes memory encodedTransferWithSender = abi.encodeWithSelector(
+            IERC20(tokenAddress).transfer.selector, 
+            recipient, 
+            amount, 
+            sender);
+
+        // call ERC20 token
+        (bool success, ) = tokenAddress.call(encodedTransferWithSender);
+
+        // emit event
+        if (success) {
+            emit TransferEvent( tokenAddress, sender, recipient, amount, metaHash);
+        }
+        return success;
+    }
+
+    function relayCall (
+        address sender,
+        address token,
+        bytes memory encodedFunctionCall, // transfer(to, amount)
+        bytes32 metaHash,
+        bytes memory metaSignature
+    ) public onlyRelayer(msg.sender) returns (bool) {
+        // TODO: verify sender's signature
+        // msg.sender is relayer, append sender to encoded function call
+        bytes memory encodedFunctionWithFrom = abi.encodePacked(encodedFunctionCall, sender);
+        (bool success, ) = token.call(encodedFunctionWithFrom);
+        if (success) {
+            emit TransactionMeta(metaHash, metaSignature);
+        }
+        return success;
+        // TODO: Event of meta data
+    }
+}
