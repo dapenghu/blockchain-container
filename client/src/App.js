73476ca4742a52047676d735157ba1b5d7ba6ef1
Dapeng Hu <damon.hu@gmail.com>
2020-01-07 01:43:23 +0800

    setup rinkeby environment

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { typedSignatureHash, concatSig, signTypedDataLegacy } from 'eth-sig-util';
 import { toBuffer, ecsign, bufferToHex } from 'ethereumjs-util';
 
@@ -17,99 +17,91 @@ import styles from './App.module.scss';
 import walletJSON from '../config/wallet_cipher.json';
 
 // eslint-disable-next-line no-unused-vars
-const infuraToken = process.env.REACT_APP_INFURA_TOKEN || '95202223388e49f48b423ea50a70e336';
-
-// const Logger = prop => (
-//   console[Object.keys(prop)[0]](...Object.values(prop)),null // ➜ React components must return something
-// )
+const INFURA_ID = process.env.REACT_APP_INFURA_TOKEN || '95202223388e49f48b423ea50a70e336';
+const RELAYER = '64cfad30f4e128568742b37b073314e5447d493f';
+const ADDRESSES = [
+  '0xb5b6baa1df2a7690e106af02cf0365f79838cdb2',
+  '0x774b0a2f952c06bab17f5e4702516ea71e4bffad',
+  '0xf731579cd24dffddf5dc34a185f51f9f72b7da1b',
+];
 
 function App() {
-  // get ephemeralKey
-  // eslint-disable-next-line no-unused-vars
-  const signKey = useEphemeralKey();
-
-  // get GSN web3 against rinkeby network
-  // const gsnContext = useWeb3Network(`wss://rinkeby.infura.io/ws/v3/${infuraToken}`, {
-  const web3Context = useWeb3Network(`http://127.0.0.1:8545`, {
-    pollInterval: 15 * 1000,
-  });
-
+  // load metamask web3 context
   const metaMaskContext = useWeb3Injected();
-  const [metamaskAccount, setMetamaskAccount] = useState(undefined);
-  if (!metamaskAccount) {
-    if (metaMaskContext.accounts && metaMaskContext.accounts.length > 0) {
-      setMetamaskAccount(metaMaskContext.accounts[0]);
-    }
-  }
+  const { accounts, networkId, networkName, lib: web3, connected } = metaMaskContext;
 
   // console.log(metaMaskContext);
-  var injectedWallet = metaMaskContext.lib.eth.accounts.wallet;
+  var injectedWallet = web3.eth.accounts.wallet;
   injectedWallet.decrypt(walletJSON, 'test');
 
-  // define token contract state
+  // load Container contract state
+  const [containerAddress, setContainerAddress] = useState(undefined);
+  const [containerInstance, setContainerInstance] = useState(undefined);
+  // load container instance
+  if (metaMaskContext && networkId && !containerInstance) {
+    let json;
+    try {
+      json = require('../../contracts/container/Container.sol');
+      var address = json.networks[networkId.toString()].address;
+      setContainerAddress(address);
+      setContainerInstance(new web3.eth.Contract(json.abi, address));
+    } catch (e) {}
+  }
+
+  // load Token instance
   const [tokenJson, setTokenJson] = useState(undefined);
   const [tokenAddress, setTokenAddress] = useState(undefined);
   const [tokenInstance, setTokenInstance] = useState(undefined);
 
-  // load Token instance
-  if (web3Context && web3Context.networkId && !tokenJson) {
+  if (metaMaskContext && networkId && !tokenInstance) {
     let json;
     try {
-      json = require('../../contracts/GaslessToken.sol');
+      json = require('../../contracts/Token/ContainerManagedERC20.sol');
     } catch (e) {}
-    var address = json.networks[web3Context.networkId.toString()].address;
+    var address = json.networks[networkId.toString()].address;
+    console.dir(json);
     setTokenJson(json);
     setTokenAddress(address);
-    setTokenInstance(new web3Context.lib.eth.Contract(json.abi, address));
+    setTokenInstance(new web3.eth.Contract(json.abi, address));
   }
 
   // state: balance sheet
-  const [addresses, setAddresses] = useState([]);
   // const [ethBalances, setEthBalances] = useState([]);
   const [tokenBalances, setTokenBalances] = useState([]);
 
   const refreshBalance = useCallback(async () => {
-    var addrs = [];
     // var ethValues = [];
     var tokenValues = [];
 
-    for (var item of walletJSON) {
-      addrs.push('0x' + item.address);
-      // var wei = await web3Context.lib.eth.getBalance(item.address);
+    for (var addr of ADDRESSES) {
+      // var wei = await metaMaskContext.lib.eth.getBalance(item.address);
       // ethValues.push(
-      //     web3Context.lib.utils.fromWei(wei, 'ether'));
-      tokenValues.push(await tokenInstance.methods.balanceOf(item.address).call());
+      //     metaMaskContext.lib.utils.fromWei(wei, 'ether'));
+      var value = await tokenInstance.methods.balanceOf(addr).call();
+      tokenValues.push(value / 100);
     }
 
-    setAddresses(addrs);
     // setEthBalances(ethValues);
     setTokenBalances(tokenValues);
   }, [tokenInstance]);
 
-  if (addresses.length == 0 && tokenInstance) {
+  if (tokenBalances.length == 0 && tokenInstance) {
     refreshBalance();
   }
 
-  // define relayer contract state
-  const [relayerAddress, setRelayerAddress] = useState(undefined);
-  const [relayerInstance, setRelayerInstance] = useState(undefined);
-  // load relayer instance
-  if (web3Context && web3Context.networkId && !relayerInstance) {
-    let json;
-    try {
-      json = require('../../contracts/Relayer1.sol');
-      var address = json.networks[web3Context.networkId.toString()].address;
-      setRelayerAddress(address);
-      setRelayerInstance(new web3Context.lib.eth.Contract(json.abi, address));
-    } catch (e) {}
-  }
+  const [sending, setSending] = useState(false);
+  const [transactionHash, setTransactionHash] = useState('');
 
-  const transfer = useCallback(
-    async (sender, recipient, amount) => {
-      var from = '0x3E88578846d48475C7864C29585509F75F875857';
-      // console.log("account : " + from);
+  const relayTransfer = useCallback(
+    async (recipient, amount) => {
+      setSending(true);
+      setTransactionHash(undefined);
 
-      // sign the message
+      const { accounts } = metaMaskContext;
+      var from = web3.utils.toHex(accounts[0]); // metamask account
+      console.log('send ' + amount + ' DCEP from ' + from + ' to recipient ' + recipient);
+
+      // sign the transfer message through Metamask
       var typedMsg = [
         {
           type: 'address',
@@ -123,24 +115,24 @@ function App() {
         },
       ];
       // compute message hash
-      const msgHash = typedSignatureHash(typedMsg);
-      console.log(msgHash);
+      // const msgHash = typedSignatureHash(typedMsg);
+      // console.log(msgHash);
 
-      // compute signature
-      const hashBuffer = toBuffer(msgHash);
-      var privateKey = toBuffer('0x768A5A8973CEAA33EBFB346EC62CBA6A3377F8EEE260C5DAD19C96C7F45CA72E');
+      // compute signature off-line
+      // const hashBuffer = toBuffer(msgHash);
+      // var privateKey = toBuffer('0x768A5A8973CEAA33EBFB346EC62CBA6A3377F8EEE260C5DAD19C96C7F45CA72E'); // matamask account SK
 
-      const sig = ecsign(hashBuffer, privateKey);
+      // const sig = ecsign(hashBuffer, privateKey);
 
-      const sigHex = bufferToHex(concatSig(sig.v, sig.r, sig.s));
-      console.log('signature: ' + sigHex);
+      // const sigHex = bufferToHex(concatSig(sig.v, sig.r, sig.s));
+      // console.log('signature: ' + sigHex);
 
       var method = 'eth_signTypedData';
       var params = [typedMsg, from];
 
-      console.dir(metaMaskContext.lib.currentProvider);
-      // sign the message
-      metaMaskContext.lib.currentProvider.sendAsync(
+      // console.dir(metaMaskContext.lib.currentProvider);
+      // sign the message with MetaMask
+      web3.currentProvider.sendAsync(
         {
           method,
           params,
@@ -149,50 +141,68 @@ function App() {
         function(err, result) {
           if (err) {
             alert('failed to sign the transaction.');
+            setSending(false);
             return;
           }
           console.log('Metamask signature:' + result.result);
 
-          // send relay transaction
-          var abi = metaMaskContext.lib.eth.abi;
-          var encodedFunctionCall = abi.encodeFunctionCall(
-            {
-              name: 'transfer',
-              type: 'function',
-              inputs: [
-                {
-                  name: 'recipient',
-                  type: 'address',
-                },
-                {
-                  name: 'amount',
-                  type: 'uint256',
-                },
-              ],
-            },
-            [recipient, amount],
-          );
+          // send relay transfer transaction to container
+          // var abi = web3.eth.abi;
+          // var encodedFunctionCall = abi.encodeFunctionCall(
+          //   {
+          //     name: 'transfer',
+          //     type: 'function',
+          //     inputs: [
+          //       {
+          //         name: 'recipient',
+          //         type: 'address',
+          //       },
+          //       {
+          //         name: 'amount',
+          //         type: 'uint256',
+          //       },
+          //     ],
+          //   },
+          //   [recipient, amount],
+          // );
 
-          relayerInstance.methods
-            .relayCall(
-              sender,
-              tokenAddress,
-              encodedFunctionCall,
-              '0x80afcdeed5a015c9d0a2c3a3fe189861907de79b30e39ca6ca5ac2bf485be661',
-              result.result,
-            )
+          console.log('transfer from ' + from + ' to ' + recipient + ' : ' + amount);
+          containerInstance.methods
+            .relayTransfer(tokenAddress, from, recipient, amount)
             .send({
-              from: '0xFaA53Ab529168732948508e03c0ab1fDDd3B91B2',
+              from: RELAYER,
               gas: 1000000,
             })
-            .then(function(receipt) {
-              console.log(receipt);
+            .on('transactionHash', function(hash) {
+              console.dir(hash);
+              setTransactionHash(hash);
+            })
+            .on('receipt', function(receipt) {
+              console.dir(receipt);
+              setSending(false);
               refreshBalance();
             });
+
+          // containerInstance.methods
+          //   .relayCall(
+          //     from,
+          //     tokenAddress,
+          //     encodedFunctionCall,
+          //     '0x80afcdeed5a015c9d0a2c3a3fe189861907de79b30e39ca6ca5ac2bf485be661',
+          //     result.result,
+          //   )
+          //   .send({
+          //     from: '0x64cFad30F4E128568742B37b073314E5447D493F', // relayer
+          //     gas: 1000000,
+          //   })
+          //   .then(function(receipt) {
+          //     console.log(receipt);
+          //     refreshBalance();
+          //   });
         },
       );
     },
-    [metaMaskContext, relayerInstance, tokenAddress, refreshBalance],
+    [metaMaskContext, web3.utils, web3.currentProvider, containerInstance.methods, tokenAddress, refreshBalance],
   );
 
   function renderNoWeb3() {
@@ -209,21 +219,27 @@ function App() {
       <Header context={metaMaskContext} />
       {/**<Hero />*/}
       <div className={styles.wrapper}>
-        {!web3Context.lib && renderNoWeb3()}
+        {!metaMaskContext.lib && renderNoWeb3()}
         <div className={styles.contracts}>
           <h1>断直连交易</h1>
-          {/**<Web3Info title="Web3 Information" context={web3Context}/>*/}
+          {/**<Web3Info title="Web3 Information" context={metaMaskContext}/>*/}
           <div className={styles.widgets}>
             {/**<AddressBook addresses={addresses} balances={ethBalances}/>*/}
             <Token
-              {...web3Context}
+              {...metaMaskContext}
               JSON={tokenJson}
               tokenAddress={tokenAddress}
               instance={tokenInstance}
-              addresses={addresses}
+              addresses={ADDRESSES}
               balances={tokenBalances}
             />
-            <Transaction {...web3Context} token={tokenInstance} transfer={transfer} />
+            <Transaction
+              {...metaMaskContext}
+              token={tokenInstance}
+              transfer={relayTransfer}
+              sending={sending}
+              txHash={transactionHash}
+            />
           </div>
         </div>
       </div>
